e523d732af7fc09e902cc1ff3217ca986a23995a
Dmitry Buravtsov <brvtsv@gmail.com>
2024-10-12 19:28:29 +0200

    styles wip

--- src/features/Menu/Menu.tsx
+++ src/features/Menu/Menu.tsx
@@ -13,16 +13,16 @@ export const Menu = () => {
         <div className={styles.wrapper}>
             <div className={styles.container}>
                 <Link href={'/fish'} className={`${styles.item} ${pathname === '/fish' ? styles.active : ''}`}>
-                    <Fish size={32} /> <p>Fish</p>
+                    <Fish size={32}/> <p className={styles.textShort}>Fish</p><p className={styles.text}>Alarm Clock Fish</p>
                 </Link>
                 <Link href={'/bird'} className={`${styles.item} ${pathname === '/bird' ? styles.active : ''}`}>
-                    <Bird size={32} /> <p>Bird</p>
+                    <Bird size={32}/> <p className={styles.textShort}>Bird</p><p className={styles.text}>Black Bird</p>
                 </Link>
                 <Link href={'/rabbit'} className={`${styles.item} ${pathname === '/rabbit' ? styles.active : ''}`}>
-                    <Rabbit size={32} /> <p>Rabbit</p>
+                    <Rabbit size={32}/> <p className={styles.textShort}>Rabbit</p><p className={styles.text}>White Rabbit</p>
                 </Link>
                 <Link href={'/rat'} className={`${styles.item} ${pathname === '/rat' ? styles.active : ''}`}>
-                    <Rat size={32}/> <p>Rat</p>
+                    <Rat size={32}/> <p className={styles.textShort}>Rat</p><p className={styles.text}>Red Rat</p>
                 </Link>
             </div>
         </div>
